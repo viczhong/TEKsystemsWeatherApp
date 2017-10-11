7ec5bac13e3d048296f439b0b7554daf431a9c6a
Victor Zhong <viczhong@gmail.com>
2017-10-11 16:20:34 -0400

    Set up file structure and API manager and other import for fake network testing, added test for Open Weather API.

---
+++ TEKsystemsWeatherApp/Models/Weather.swift
@@ -0,0 +1,14 @@
+//
+//  Weather.swift
+//  TEKsystemsWeatherApp
+//
+//  Created by Victor Zhong on 10/10/17.
+//  Copyright © 2017 Victor Zhong. All rights reserved.
+//
+
+import Foundation
+
+enum WeatherModelParseError: Error {
+    case results, parsing
+}
+
